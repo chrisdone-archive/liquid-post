0557b673d3bbcde894d83cf672f984106cefdfa1
Chris Done <chrisdone@gmail.com>
2017-03-12 18:18:48 +0000

    Tidy Date

--- examples/Date.hs
+++ examples/Date.hs
@@ -2,31 +2,27 @@
 
 module Date where
 
-data Date = Date { day :: !Int, month :: !Int, year :: !Int }
-  deriving (Show)
+{-@ type Day =
+  {v:Int | v > 0 && v <= 31 } @-}
 
-{-@
-makeDate :: day:{Int | day > 0 && day <= 31 }
-         -> month:{Int | month > 0 && month <= 12 && (day < 31 || month = 04 || month = 06 || month = 09 || month = 11) }
-         -> { year:Int | year > 0 &&
-              (month /= 2 ||
-               (day < 29 ||
-                year mod 400 = 0 || (year mod 4 = 0 && year mod 100 /= 0)
-               )
-              )
-            }
-         -> Date
-@-}
-makeDate :: Int -> Int -> Int -> Date
-makeDate d m y = Date d d y
+{-@ type Month day =
+  {v:Int | v > 0 && v <= 12 && (day < 31 || v = 04 || v = 06 || v = 09 || v = 11) } @-}
 
-works = makeDate 12 03 2017
-works_leap_day = makeDate 29 02 2016
--- Does not compile:
--- invalid_nov_day = makeDate 11 31 2017
--- invalid_month = makeDate 12 15 2017
--- invalid_leap_day = makeDate 29 02 2017
+{-@ type Year month day =
+  {v:Int | v > 0 && (month /= 2 || (day < 29 || v mod 400 = 0 || (v mod 4 = 0 && v mod 100 /= 0))) } @-}
 
+data Date = Date
+  { day :: !Int
+  , month :: !Int
+  , year :: !Int
+  } deriving (Show)
+{-@ data Date = Date
+  { day   :: Day
+  , month :: Month day
+  , year  :: Year month day
+  } @-}
+
+main :: IO ()
 main = do
   year :: Int <- readLn
   month :: Int <- readLn
@@ -34,7 +30,7 @@ main = do
   if year > 0
     then if month > 0 && month <= 12 && day < 31 || month == 04 || month == 06 || month == 09 || month == 11
            then if day > 0 && day <= 30 && valid_leap_days day month year
-                  then print (makeDate day month year)
+                  then print (Date day month year)
                   else error "Day is out of range!"
            else error "Month is out of range."
     else error "Year is out of range."
@@ -42,3 +38,15 @@ main = do
     valid_leap_days day month year =
       (month /= 2 ||
        (day < 29 || mod year 400 == 0 || (mod year 4 == 0 && mod year 100 /= 0)))
+
+
+works :: Date
+works = Date 12 03 2017
+
+works_leap_day :: Date
+works_leap_day = Date 29 02 2016
+
+-- Does not compile:
+-- invalid_nov_day = Date 11 31 2017
+-- invalid_month = Date 12 15 2017
+-- invalid_leap_day = Date 29 02 2017
